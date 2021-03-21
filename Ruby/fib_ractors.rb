355244f8c05b9a94c6eec013b8d374d2fb4a9b2d
Guillermo Lella <arkorott@gmail.com>
2021-03-21 00:45:54 -0400

    update

--- Ruby/fib_ractors.rb
+++ Ruby/fib_ractors.rb
@@ -1,8 +1,12 @@
 #ruby --jit-wait fib.rb
 
 def fib(n)
-  	return 1 if n <= 1
-  	fib(n - 1) + fib(n - 2)
+  return 1 if n <= 1
+  ractors = []
+  ractors << Ractor.new do
+  	
+  end
+  fib(n - 1) + fib(n - 2)
 end
 
 start_time = Time.now
